c2e37e403ce32ccd911a6079b6b1582143de1707
Sander <sandervandenoetelaar@hotmail.com>
2020-11-20 22:31:36 +0100

    Removed boilerplate code

---
+++ app/src/main/java/nl/svdoetelaar/madlevel7example/CreateQuizFragment.kt
@@ -0,0 +1,22 @@
+package nl.svdoetelaar.madlevel7example
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CreateQuizFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_create_quiz, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
