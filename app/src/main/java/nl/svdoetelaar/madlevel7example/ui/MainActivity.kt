38a38974e651686d92b1390ffb8d9f8235dee6ba
Sander <sandervandenoetelaar@hotmail.com>
2020-11-23 10:40:23 +0100

    4.1 firebase

--- app/src/main/java/nl/svdoetelaar/madlevel7example/ui/MainActivity.kt
+++ app/src/main/java/nl/svdoetelaar/madlevel7example/ui/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
 import nl.svdoetelaar.madlevel7example.R
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        FirebaseFirestore.setLoggingEnabled(true)
+        FirebaseApp.initializeApp(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/nl/svdoetelaar/madlevel7example/ui/QuizFragment.kt
+++ app/src/main/java/nl/svdoetelaar/madlevel7example/ui/QuizFragment.kt
@@ -1,10 +1,11 @@
-package nl.svdoetelaar.madlevel7example
+package nl.svdoetelaar.madlevel7example.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import nl.svdoetelaar.madlevel7example.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,10 +30,8 @@ class QuizFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_quiz, container, false)
     }
@@ -47,13 +46,12 @@ class QuizFragment : Fragment() {
          * @return A new instance of fragment QuizFragment.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            QuizFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        @JvmStatic fun newInstance(param1: String, param2: String) =
+                QuizFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
